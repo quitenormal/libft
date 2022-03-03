30aa924d8f7d55aa09c5e41d4eae910359796c16
quitenormal <daojira11@gmail.com>
2022-03-04 06:01:50 +0700

    Me is elmo and frieds

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yjirapin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/24 13:42:16 by yjirapin          #+#    #+#             */
-/*   Updated: 2022/03/04 06:01:37 by yjirapin         ###   ########.fr       */
+/*   Created: 2022/03/04 05:57:39 by yjirapin          #+#    #+#             */
+/*   Updated: 2022/03/04 05:59:02 by yjirapin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int coldplay);
-int	ft_strlen(char *str);
-int	ft_isascii(int chill);
-int	ft_isprint(int cicada);
-
-#endif
+int	ft_isascii(int chill)
+{
+	if (chill >= 0 && chill <= 127)
+		return (1);
+	else
+		return (0);
+}
--- ft_isprint.c
+++ ft_isprint.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yjirapin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/24 13:42:16 by yjirapin          #+#    #+#             */
-/*   Updated: 2022/03/04 06:01:37 by yjirapin         ###   ########.fr       */
+/*   Created: 2022/03/04 05:59:25 by yjirapin          #+#    #+#             */
+/*   Updated: 2022/03/04 06:00:11 by yjirapin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int coldplay);
-int	ft_strlen(char *str);
-int	ft_isascii(int chill);
-int	ft_isprint(int cicada);
-
-#endif
+int ft_isprint(int cicada)
+{
+	if (cicada >= 32 && cicada <= 126)
+    	return (1);
+    else
+    	return (0);
+}
